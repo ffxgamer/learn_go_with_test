c150bffb1c5f6f5b58c2e26d71de68177093b616
彭文明 <peng.wenming@synyi.com>
2020-11-10 13:07:56 +0800

    增加ExampleRepeat

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 4)
@@ -15,4 +18,9 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 4)
 	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("b", 10))
+	// output: "bbbbbbbbbb"
 }
